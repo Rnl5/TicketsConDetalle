24c4b3c9fa5855644b729ba6514b13b52ab4e8c7
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-10-23 14:43:08 -0400

    Agregando nueva migracion y actualizando la base de datos

---
+++ Server/Migrations/20231023183733_NuevaValidacionEnTickets.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketsConDetalle.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class NuevaValidacionEnTickets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
